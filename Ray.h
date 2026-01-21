405501be284f2bbfdef50e5f7517f84b1d4c6a66
DiegoH81 <hidal8909ins@gmail.com>
2026-01-21 14:14:51 -0500

    feat(ray): Add ray class

--- Ray.h
+++ Ray.h
@@ -4,19 +4,24 @@
 
 #include "Vec3.h"
 
-using color = Vec3;
 
-void write_color(std::ostream& out, color in_pixel_color)
+class Ray
 {
-	auto& r = in_pixel_color.x;
-	auto& g = in_pixel_color.y;
-	auto& b = in_pixel_color.z;
+public:
+	Ray(const Vec3& in_vector, const Point3& in_origin) :
+		m_vector(in_vector), m_origin(in_origin) {}
 
-	double out_r = 255.9 * r;
-	double out_g = 255.9 * g;
-	double out_b = 255.9 * b;
+	const Point3& origin() { return m_origin; };
+	const Point3& vector() { return m_vector; };
 
-	out << out_r << " " << out_g << " " << out_b << "\n";
-}
+
+	Point3 at(const double& in_distance) const
+	{
+		return m_origin + m_vector * in_distance;
+	}
+private:
+	Vec3 m_vector;
+	Point3 m_origin;
+};
 
 #endif#pragma once
