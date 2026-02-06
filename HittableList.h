fd8e299b76208c9e103502f96c84c5142537b30b
DiegoH81 <hidal8909ins@gmail.com>
2026-02-06 17:47:03 -0500

    feat(Hittable list): Add hittable list

---
+++ HittableList.h
@@ -0,0 +1,52 @@
+#pragma once
+#ifndef HITTABLE_LIST_H
+#define HITTABLE_LIST_H
+
+#include "Hittable.h"
+#include <memory>
+#include <vector>
+
+class HittableList : public Hittable
+{
+public:
+	std::vector<std::shared_ptr<Hittable>> objects;
+
+
+	HittableList(): objects() {}
+	HittableList(std::shared_ptr<Hittable> in_obj)
+	{
+		add(in_obj);
+	}
+
+	void add(std::shared_ptr<Hittable> in_obj)
+	{
+		objects.push_back(in_obj);
+	}
+
+	void clear()
+	{
+		objects.clear();
+	}
+
+	bool hit(const Ray& in_ray, double t_min, double t_max, HitRecord& in_record) override
+	{
+		HitRecord temp_rec;
+		bool hit_anything{ false };
+		double closest_so_far = t_max;
+
+		for (auto& object : objects)
+		{
+			if (object->hit(in_ray, t_min, closest_so_far, temp_rec))
+			{
+				hit_anything = true;
+				closest_so_far = temp_rec.t;
+				in_record = temp_rec;
+			}
+		}
+
+		return true;
+	}
+};
+
+
+#endif
